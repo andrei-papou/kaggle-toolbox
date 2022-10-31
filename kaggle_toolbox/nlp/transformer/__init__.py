c616754740dcc2ddd4accd3d38f541fb16e7369d
andrei.papou <andrei.v.papou@gmail.com>
2022-10-31 17:39:15 +0300

    Fixed `__init__.py` module of `nlp.transformer`.

--- kaggle_toolbox/nlp/transformer/__init__.py
+++ kaggle_toolbox/nlp/transformer/__init__.py
@@ -1,8 +1,8 @@
 from .backbone import Backbone
 from .data import TokenizerResultCollator
 from .initialization import standard_init_linear, standard_init_layer_norm
-from .model import Model, StandardModel, ClsTokenPooler, AttentionHeadPooler, MeanPooler, \
-    TakeNthHiddenLayerOutputSqueezer
+from .model import Model, Pooler, Squeezer, StandardModel, ClsTokenPooler, AttentionHeadPooler, \
+    MeanPooler, TakeNthSqueezer, SumSqueezer, MeanSqueezer, ConcatSqueezer
 from .optim import create_nakama_optimizer
 from .seed import seed_everything
 from .tokenization import Tokenizer, TokenizerResult, get_tokenizer_for_backbone
