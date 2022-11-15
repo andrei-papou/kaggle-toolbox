c228e706495284a5148f6f56e9a0fc464b28bff9
andrei.papou <andrei.v.papou@gmail.com>
2022-11-15 17:30:28 +0300

    `FuncList` feature generator + more tests.

--- tests/features/test_generation.py
+++ tests/features/test_generation.py
@@ -1,9 +1,9 @@
-import math
+import typing as t
 
 import numpy as np
 
 from kaggle_toolbox.features.generation import Mean, Stdev, \
-    FuncBinaryOp
+    FuncBinaryOp, FuncList
 
 
 def test_mean():
@@ -23,11 +23,28 @@ def test_stdev():
         name='mean_score',
         feature_list=['fold_0_score', 'fold_1_score', 'fold_2_score'])
     feature_arr = gen({
+        'fold_0_score': np.array([0.0, 1.0]),
+        'fold_1_score': np.array([3.0, 2.0]),
+        'fold_2_score': np.array([6.0, 3.0]),
+    })
+    assert (
+        feature_arr == np.array([
+            np.array([0.0, 3.0, 6.0]).std(),
+            np.array([1.0, 2.0, 3.0]).std(),
+        ])).all()
+
+
+def test_func_list():
+    gen = FuncList(
+        name='custom_mean',
+        feature_list=['fold_0_score', 'fold_1_score', 'fold_2_score'],
+        func=lambda xs: t.cast(np.ndarray, sum(xs.values()) / len(xs)))
+    feature_arr = gen({
         'fold_0_score': np.array([0.0]),
-        'fold_1_score': np.array([3.0]),
+        'fold_1_score': np.array([6.0]),
         'fold_2_score': np.array([6.0]),
     })
-    assert math.isclose(feature_arr, np.array([np.array([0.0, 3.0, 6.0]).std()]))
+    assert feature_arr == np.array([4.0])
 
 
 def test_func_binary_op():
