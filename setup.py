436ac31e6787b70ed97df520d65e01ffb527f55d
andrei.papou <andrei.v.papou@gmail.com>
2022-11-16 15:54:31 +0300

    Before optimizer step hook + grad clip.

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ requirement_list = core_requirement_list
 
 setuptools.setup(
     name='kaggle_toolbox',
-    version='0.1.4',
+    version='0.1.5',
     description='Toolbox library for Kaggle competitions.',
     long_description=long_description,
     long_description_content_type='text/markdown',
