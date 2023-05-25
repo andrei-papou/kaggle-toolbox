b7984c5643040d32b334bf1d7587b5754555b6c6
andrei.papou <andrei.v.papou@gmail.com>
2023-05-25 16:21:09 +0300

    Tokenization submodule reworked

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ requirement_list = core_requirement_list
 
 setuptools.setup(
     name='kaggle_toolbox',
-    version='0.3.1',
+    version='0.4.0',
     description='Toolbox library for Kaggle competitions.',
     long_description=long_description,
     long_description_content_type='text/markdown',
